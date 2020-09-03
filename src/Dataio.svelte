ec65cb9c4a4e069c72746658109558dcb01bb430
NabiYi <jaehyun618@naver.com>
2020-09-04 05:36:25 +0900

    change common idpw

--- src/Dataio.svelte
+++ src/Dataio.svelte
@@ -2,8 +2,8 @@
   import neo4j from "../node_modules/neo4j-driver/lib/browser/neo4j-web.js";
 
   const uri = "bolt://localhost:7687";
-  const user = "nabi";
-  const password = "1212";
+  const user = "neo4j";
+  const password = "neo4j";
   const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
   const session = driver.session();
 
